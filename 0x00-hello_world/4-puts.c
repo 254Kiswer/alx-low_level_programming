5d19067354d64dd1941d95754b99ad4fec056b6f
254Kiswer <254Kiswer@github.com>
2022-09-08 10:33:56 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * return: (0)
+ */
 
 int main(void)
 {
