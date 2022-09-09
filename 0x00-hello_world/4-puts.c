cd89d21c364a2832cb4bc7129660bc015b230f33
254Kiswer <254Kiswer@github.com>
2022-09-09 04:46:44 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - task 4
+ *
+ * Return: 0 after printing text
+ **/
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
