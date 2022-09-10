b7e7ef24edf72c579df7f43f4f5e2572255ab2d8
254Kiswer <254Kiswer@github.com>
2022-09-10 07:41:04 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,15 +13,16 @@ int main(void)
 {
 	int num;
 	for (num = 48; num <= 57; num++)
-}
-putchar (num);
-if (num == 57)
+{
+	putchar (num);
+
+	if (num == 57)
 {
 	break;
 }
-putchar (',');
-putchar (',');
+	putchar (',');
+	putchar (',');
 }
-putchar ('\n');
+	putchar ('\n');
 return (0);
 }
