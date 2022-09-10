e6f19ae230a651d98f750785670ac770a1ca4cfa
254Kiswer <254Kiswer@github.com>
2022-09-10 06:26:07 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here*/
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - task 5
+ * Return: 0 on succes
+ * */
+
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+printf("%d", x0);
+}
+print("\n");
+return (0);
+}
