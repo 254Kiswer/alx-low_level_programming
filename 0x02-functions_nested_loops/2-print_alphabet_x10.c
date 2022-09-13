736457d63baf2b0ec2c3fe825cceabef5370e826
254Kiswer <254Kiswer@github.com>
2022-09-13 08:23:54 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * function prints alphabets 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for(x = 0; x < 10; x++)
+	{
+
+		for (i = 'a'; i <='z' ; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+	}
