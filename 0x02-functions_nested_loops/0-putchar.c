c82b6759b7c3d16148c1a3a958f0dca74a1f27ea
254Kiswer <254Kiswer@github.com>
2022-09-13 06:43:04 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - task 0
+ * main - task 0 prints _putchar
  * Return: 0 
  */
 
 int main(void)
 {
-	putchar("_putchar");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 return (0)
 }
