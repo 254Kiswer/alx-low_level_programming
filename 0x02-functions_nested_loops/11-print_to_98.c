bbadf82c5f4e2cc3db769da5be23b12a787f2730
254Kiswer <254Kiswer@github.com>
2022-09-14 07:56:34 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * priny_to_98 - print all natural numbers from n to 98
+ * print_to_98 - print all natural numbers from n to 98
  * @n: int type number
  */
 void print_to_98(int n)
