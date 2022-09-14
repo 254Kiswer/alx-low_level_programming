c4d741a6c122f964cde8ae33b9a3658834f696bd
254Kiswer <254Kiswer@github.com>
2022-09-14 03:27:16 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdioh>
+
+/**
+ * add - Add two integers
+ * @X: int type number
+ * @Y: int type number
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
