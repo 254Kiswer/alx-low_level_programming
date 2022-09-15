2bd06e594b920340261261831ca50c107ea3cfc4
254Kiswer <254Kiswer@github.com>
2022-09-15 08:56:10 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,15 @@ void print_square(int size)
 
 		if (size <= 0)
 		{
-					_putchar('\n');
+			_putchar('\n');
 		}
 		else
 		{
 			for (i = 1; i <= size; i++)
 			{
-			_putchar('#');								for (j = 1; j < size; j++)
+			_putchar('#');
+
+			for (j = 1; j < size; j++)
 					{
 						_putchar('#');
 					}
