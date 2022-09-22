734e1b5a1950a1abc3972e7ba7dc14fd75a2dcc1
254Kiswer <254Kiswer@github.com>
2022-09-22 04:11:43 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
-
-	result = strcmp(s1, s2);
-	return (result);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
 
