0ba8171dfd4a05dcacfbb05c6dac23bcea3aa483
254Kiswer <254Kiswer@github.com>
2022-09-26 06:50:18 -0700

    9. My primary goal of hacking was the intellectual curiosity, the seduction of adventure

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strchr - search the first occurance of the charater in a string pointed
+ * @s: string pointed
+ * @c: character to search
+ * Return: s ( the first occurence of the char)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
