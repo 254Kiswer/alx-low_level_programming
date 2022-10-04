5fc78c13fcffca535419ba9eb2289f1ff1933c32
254Kiswer <254Kiswer@github.com>
2022-10-04 04:36:42 -0700

    hotfix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,16 @@
 int main(int argc, char *argv[])
 {
 	int mul;
+
 	if (argc == 3)
-		{
-			mul = atoi(argv[1] * atoi(argv[2]);
-					printf("%d\n", mul);
-					}
-					else
-					{
-					printf("Error\n");
-					return (1);
-					}
-					return (0);
-					}
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
